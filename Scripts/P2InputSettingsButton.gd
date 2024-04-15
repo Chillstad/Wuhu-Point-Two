941a2c5ea2f31d3467c47b978b9e5be50911f55f
Cash <cashhilstad@gmail.com>
2024-04-15 09:23:32 -0700

    Wunkiness fixed

--- Scripts/P2InputSettingsButton.gd
+++ Scripts/P2InputSettingsButton.gd
@@ -1,12 +1,10 @@
 extends OptionButton
 
-@onready var input_settings = get_node("/root/InputSettings")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(0, input_settings.input_options.size()):
-		add_item(input_settings.input_options[i], i)
-	select(input_settings.player_2_input_device_index)
+	for i in range(0, InputSettings.input_options.size()):
+		add_item(InputSettings.input_options[i], i)
+	select(InputSettings.player_2_input_device_index)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,5 +13,5 @@ func _process(delta):
 
 
 func _on_item_selected(index):
-	input_settings.setPlayer2(index)
+	InputSettings.setPlayer2(index)
 	
---
+++ Scripts/settings_screen.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_close_pressed():
+	visible = false
